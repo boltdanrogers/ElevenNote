2207e54185ff876f8cc603290154a15ef6445688
Daniel Rogers <boldanrogers@gmail.com>
2021-02-15 22:16:42 -0500

    finished with noteController

---
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,58 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    public class NoteController : ApiController
+    {
+
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new NoteService(userId);
+            return noteService;
+        
+        }//end of method CreateNoteService
+
+        //lets add a get all method
+        public IHttpActionResult Get()
+        {
+            NoteService noteService = CreateNoteService();
+            var notes = noteService.GetNotes();
+            return Ok(notes);
+
+        }//end of method Get
+
+        public IHttpActionResult Post(NoteCreate note)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }//end of if modelState is bad
+
+            var service = CreateNoteService();
+
+            if (!service.CreateNote(note))
+            {
+                return InternalServerError();
+            }//end of if not able to create note
+
+            return Ok();
+
+
+        }//end of method Post
+
+
+
+
+
+    }//end of class NoteController
+}
