f55b52a2c4e7fcb7b79c5602087ab46e8d63aaed
Daniel Rogers <boldanrogers@gmail.com>
2021-02-15 19:50:53 -0500

    finished with NoteService.cs

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,77 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId;
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+
+        }//end of constructor
+
+        //now we have a method to create a note
+
+        public bool CreateNote(NoteCreate model)
+        {
+            var entity =
+                new Note()
+                {
+                    OwnerId = _userId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };//end of new Note
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+
+            }//end of using
+
+
+        }//end of method CreateNote
+
+
+        //and now a method to get notes
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Notes
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                        new NoteListItem
+                        {
+                            NoteId = e.NoteId,
+                            Title = e.Title,
+                            CreatedUtc = e.CreatedUtc
+
+                        }
+                        );
+
+                return query.ToArray();
+
+
+            }//end of using 
+
+
+        }//end of method GetNotes
+
+
+    }//end of class NoteService
+}
